9f442f98d901685c9f83280ed9ba718f34d2af7d
abrahamh <abrahamhijazeen@gmail.com>
2016-01-04 21:38:04 +0100

    Image follows mouse first commit

---
+++ js/eyesmove.js
@@ -0,0 +1,13 @@
+$(window).load( function(){
+	
+	$(document).mousemove( function(e){
+		
+	var x = e.pageX;
+	var y = e.pageY;
+	
+	$('#image').css('margin-left', x)
+	$('#image').css('margin-left', x)
+	
+	});
+	
+});
